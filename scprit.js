cbbbf0bb923dbe7cdba4c2a75048d283a6c067ef
gabrielle costa <gabicostagaber@gmail.com>
2024-03-10 15:43:55 -0300

    javascript para abrir e fechar o menu completo

---
+++ scprit.js
@@ -0,0 +1,17 @@
+const botaoabrir = document.querySelector(".header-mobile > button")
+const botaofechar = document.querySelector(".header-mobile nav button")
+
+const body = document.querySelector("body")
+const nav = document.querySelector(".header-mobile nav")
+
+botaoabrir.addEventListener('click', abrirmenu)
+botaofechar.addEventListener('click', fecharmenu)
+function abrirmenu() {
+    body.classList.add("escurecer")
+    nav.classList.add("abrir")
+}
+
+function fecharmenu() {
+   body.classList.remove("escurecer")
+   nav.classList.remove("abrir")
+}
